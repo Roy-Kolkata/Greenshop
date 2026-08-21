0d90908ce95d4d9cad8296375ced25f10cbe1b97
Akash Roy <roy2004akash@gmail.com>
2026-08-21 13:49:12 +0530

    feat: implement shopping cart and checkout pages with geolocation integration and user authentication hook

---
+++ src/app/user/checkout/page.tsx
@@ -0,0 +1,204 @@
+"use client"
+import { RootState } from '@/redux/store'
+import axios from 'axios'
+import type { LatLngExpression, LeafletEvent } from 'leaflet'
+import "leaflet/dist/leaflet.css"
+import { ArrowLeft, Building, Home, MapPin, Navigation, Phone, Search, User } from 'lucide-react'
+import { motion } from 'motion/react'
+import { useRouter } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+
+function Checkout() {
+    const router = useRouter()
+    const { userData } = useSelector((state: RootState) => state.user)
+    const [address, setAddress] = useState({
+        fullName: "",
+        mobile: "",
+        city: "",
+        state: "",
+        pincode: "",
+        fullAddress: ""
+    })
+    const [position, setPosition] = useState<[number, number] | null>(null)
+    const [mapComponents, setMapComponents] = useState<any>(null)
+
+    useEffect(() => {
+        Promise.all([
+            import('leaflet'),
+            import('react-leaflet')
+        ]).then(([L, RL]) => {
+            const markerIcon = new L.Icon({
+                iconUrl: "https://cdn-icons-png.flaticon.com/128/684/684908.png",
+                iconSize: [40, 40],
+                iconAnchor: [20, 40]
+            })
+            setMapComponents({
+                MapContainer: RL.MapContainer,
+                TileLayer: RL.TileLayer,
+                Marker: RL.Marker,
+                useMap: RL.useMap,
+                markerIcon
+            })
+        })
+    }, [])
+
+    useEffect(() => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (pos) => {
+                    const { latitude, longitude } = pos.coords;
+                    setPosition([latitude, longitude]);
+                },
+                (err) => {
+                    console.log("location error", err);
+                },
+                {
+                    enableHighAccuracy: true,
+                    maximumAge: 0,
+                    timeout: 10000
+                }
+            );
+        }
+    }, []);
+
+    useEffect(() => {
+        if (userData) {
+            setAddress((prev) => ({
+                ...prev,
+                fullName: userData.name || "",
+                mobile: userData.mobile || ""
+            }))
+        }
+    }, [userData])
+
+    useEffect(() => {
+        const fetchAddress = async () => {
+            if (!position) return
+            try {
+                const result = await axios.get(`https://nominatim.openstreetmap.org/reverse?lat=${position[0]}&lon=${position[1]}&format=json`)
+                console.log(result.data)
+                // setAddress(prev=>({...prev,city:result.data.address.city}))
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchAddress()
+    }, [position])
+
+    const DraggableMarker: React.FC = () => {
+        if (!mapComponents) return null
+        const map = mapComponents.useMap()
+        const Marker = mapComponents.Marker
+
+        useEffect(() => {
+            if (position && map) {
+                map.setView(position as LatLngExpression, 15, { animate: true })
+            }
+        }, [map])
+
+        return (
+            <Marker
+                icon={mapComponents.markerIcon}
+                position={position as LatLngExpression}
+                draggable={true}
+                eventHandlers={{
+                    dragend: (e: LeafletEvent) => {
+                        const marker = e.target
+                        const { lat, lng } = marker.getLatLng()
+                        setPosition([lat, lng])
+                    }
+                }}
+            />
+        )
+    }
+
+    const MapContainer = mapComponents?.MapContainer
+    const TileLayer = mapComponents?.TileLayer
+
+    return (
+        <div className='w-[92%] md:w-[80%] mx-auto py-10 relative'>
+            <motion.button
+                whileHover={{ scale: 1.06 }}
+                whileTap={{ scale: 0.98 }}
+                className='absolute top-2 left-0 flex items-center gap-2 text-green-700 font-semibold bg-white px-4 py-2 rounded-full shadow-md hover:bg-green-100 hover:shadow-lg transition-all' onClick={() => router.push("/user/cart")}>
+                <ArrowLeft className='w-5 h-5' />
+                <span className='hidden md:flex'>Back to Cart</span>
+            </motion.button>
+
+            <motion.h1
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.3 }}
+                className='text-3xl md:text-4xl font-bold text-green-700 text-center mb-10'
+            >
+                Checkout
+            </motion.h1>
+
+            <div className='grid md:grid-cols-2 gap-8'>
+                <motion.div
+                    initial={{ opacity: 0, x: -20 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.3 }}
+                    className='bg-white rounded-2xl shadow-lg hover:shadow-xl transition-all duration-300 p-6 border border-gray-100'
+                >
+                    <h2 className='text-xl font-semibold text-gray-800 mb-4 flex items-center gap-2'>
+                        <MapPin className='text-green-700' /> Delivery Address
+                    </h2>
+                    <div className='space-y-4'>
+                        <div className='relative'>
+                            <User className='absolute left-3 top-3 text-green-600' size={18} />
+                            <input type="text" placeholder='Name' value={address.fullName} className='pl-10 w-full border rounded-lg p-3 text-sm bg-gray-50' onChange={(e) => setAddress((prev) => ({ ...prev, fullName: e.target.value }))} />
+                        </div>
+
+                        <div className='relative'>
+                            <Phone className='absolute left-3 top-3 text-green-600' size={18} />
+                            <input type="text" placeholder='Mobile number' value={address.mobile} className='pl-10 w-full border rounded-lg p-3 text-sm bg-gray-50' onChange={(e) => setAddress((prev) => ({ ...prev, mobile: e.target.value }))} />
+                        </div>
+
+                        <div className='relative'>
+                            <Home className='absolute left-3 top-3 text-green-600' size={18} />
+                            <input type="text" placeholder='Address' value={address.fullAddress} className='pl-10 w-full border rounded-lg p-3 text-sm bg-gray-50' onChange={(e) => setAddress((prev) => ({ ...prev, fullAddress: e.target.value }))} />
+                        </div>
+
+                        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3'>
+                            <div className='relative'>
+                                <Building className='absolute left-3 top-3 text-green-600' size={18} />
+                                <input type="text" placeholder='City' value={address.city} className='pl-10 w-full border rounded-lg p-3 text-sm bg-gray-50' onChange={(e) => setAddress((prev) => ({ ...prev, city: e.target.value }))} />
+                            </div>
+
+                            <div className='relative'>
+                                <Navigation className='absolute left-3 top-3 text-green-600' size={18} />
+                                <input type="text" placeholder='State' value={address.state} className='pl-10 w-full border rounded-lg p-3 text-sm bg-gray-50' onChange={(e) => setAddress((prev) => ({ ...prev, state: e.target.value }))} />
+                            </div>
+
+                            <div className='relative'>
+                                <Search className='absolute left-3 top-3 text-green-600' size={18} />
+                                <input type="text" placeholder='Pincode' value={address.pincode} className='pl-10 w-full border rounded-lg p-3 text-sm bg-gray-50' onChange={(e) => setAddress((prev) => ({ ...prev, pincode: e.target.value }))} />
+                            </div>
+                        </div>
+
+                        <div className='flex gap-2 mt-3'>
+                            <input type="text" placeholder='Search city or area' className='flex-1 border rounded-lg p-3 text-sm focus:ring-2 focus:ring-green-500 outline-none' />
+                            <button className='bg-green-600 text-white px-5 rounded-lg hover:bg-green-700 transition-all font-medium '>Search</button>
+                        </div>
+
+                        <div className='relative mt-6 h-82.5 rounded-xl overflow-hidden border border-gray-200 shadow-inner'>
+                            {position && MapContainer && TileLayer && (
+                                <MapContainer center={position as LatLngExpression} zoom={13} scrollWheelZoom={true} className='w-full h-full'>
+                                    <TileLayer
+                                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                    />
+                                    <DraggableMarker />
+                                </MapContainer>
+                            )}
+                        </div>
+                    </div>
+                </motion.div>
+            </div>
+        </div>
+    )
+}
+
+export default Checkout
